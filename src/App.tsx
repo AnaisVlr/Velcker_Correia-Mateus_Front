3fadb3f8643277ac942fa281403ef2ac118972ab
Dokarus <dorian.752@live.fr>
2023-03-04 11:00:55 +0100

    Ajoute un themeProvider dans App, pour la police

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,30 @@ import AddBenevole from './components/benevole/AddBenevole';
 import AddCreneau from './components/benevole/AddCreneau';
 import NavigationBar from './components/NavigationBar';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Playfair Display',
+      'serif'
+    ].join(','),
+  },
+});
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/jeux" element={<NavigationBar page="jeu"/>}/>
-      <Route path="/benevoles" element={ <NavigationBar page="benevole"/>}/>
-      <Route path="/benevoles/addBenevole" element={ <NavigationBar page="addBenevole"/> }/>
-      <Route path="/benevoles/addCreneau" element={ <NavigationBar page="addCreneau"/> }/>
-      <Route path="/zones" element={<NavigationBar page="zone"/>}/>
-      <Route path="/profil/:user" element={ <NavigationBar page="profil"/> }/>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/jeux" element={<NavigationBar page="jeu"/>}/>
+        <Route path="/benevoles" element={ <NavigationBar page="benevole"/>}/>
+        <Route path="/benevoles/addBenevole" element={ <NavigationBar page="addBenevole"/> }/>
+        <Route path="/benevoles/addCreneau" element={ <NavigationBar page="addCreneau"/> }/>
+        <Route path="/zones" element={<NavigationBar page="zone"/>}/>
+        <Route path="/profil/:user" element={ <NavigationBar page="profil"/> }/>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
